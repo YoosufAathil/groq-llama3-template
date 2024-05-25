d9eb9c25eb84dac2bf7ad9baf718596a2330a79c
Yoosuf Aathil <117569200+YoosufAathil@users.noreply.github.com>
2024-05-25 16:55:34 +0000

    Add chatbot functionality with user input

--- hello_groq.py
+++ hello_groq.py
@@ -1,7 +1,5 @@
 import os
-
 from groq import Groq
-
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -10,15 +8,25 @@ client = Groq(
     api_key=os.environ.get("GROQ_API_KEY"),
 )
 
-user_input = input("Enter your message: ")
-chat_completion = client.chat.completions.create(
-    messages=[
-        {
-            "role": "user",
-            "content": user_input,
-        }
-    ],
-    model="llama3-8b-8192",
-)
+def chatbot():
+    print("Hello! I'm your chatbot. You can ask me questions. If you want to exit, type 'bye'.")
+    
+    while True:
+        user_input = input("Enter your message: ")
+        
+        if user_input.lower() == 'bye':
+            break
 
-print(chat_completion.choices[0].message.content)
+        chat_completion = client.chat.completions.create(
+            messages=[
+                {
+                    "role": "user",
+                    "content": user_input,
+                }
+            ],
+            model="llama3-8b-8192",
+        )
+
+        print(chat_completion.choices[0].message.content)
+
+chatbot()
